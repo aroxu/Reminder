7a20b3b8bf7191de493daa2fa791919bae517bc6
B1ackAnge1 <30624945+B1ackAnge1@users.noreply.github.com>
2020-06-20 18:03:28 +0900

    Initial Commit

---
+++ reminderTests/reminderTests.swift
@@ -0,0 +1,34 @@
+//
+//  reminderTests.swift
+//  reminderTests
+//
+//  Created by 윤규도 on 2020/06/20.
+//  Copyright © 2020 B1ackAnge1. All rights reserved.
+//
+
+import XCTest
+@testable import reminder
+
+class reminderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
